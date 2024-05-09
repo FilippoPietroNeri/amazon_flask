a1075cf1ab1a3b73074c6ac22c3c9372676ba50c
FilippoPietroNeri <neri.filippo@iisgalvanimi.edu.it>
2024-05-09 09:48:17 +0000

    added others, must fix bug in click

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ df = pd.read_csv('prodotti.csv')
 def homepage():
     return render_template('index.html', products=df.to_dict('records'))
 
-@app.route('/product/get')
+@app.route('/product/get', methods=['POST'])
 def products():
-    return {}
+    if request.method == "POST":
+        json = request.get_json()
+        productName = json.get('productName')
+        getData = df[df['Nome'] == productName]
+        return { "code": 200, "data": getData.to_json() }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,37 @@
-function getProduct()
+var maincontainer = document.getElementById('maincontainer');
+var infocontainer = document.getElementById('infocontainer');
+
+// INFO CONTAINER THINGS
+var product_title = document.querySelector('#product-title');
+var product_image = document.querySelector('#product-image');
+var prezzo = document.querySelector('#prezzo');
+var quantita = document.querySelector('#quantita');
+var categoria = document.querySelector('#categoria');
+
+function getProduct(name)
 {
-    
+    maincontainer.style.display = "none";
+    infocontainer.style.display = "block";
+    console.log(name)
+    product_title.innerHTML = name;
+    fetch('https://3245-filippopiet-amazonflask-ilx4h04ao05.ws-eu111.gitpod.io/product/get', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({productName: name}),
+    }).then((response) => response.json())
+    .then((data) => {
+        const productData = JSON.parse(data.data);
+        product_image.src = productData['Immagine'][0];
+        prezzo.innerHTML = `${productData['Prezzo'][0]}€`;
+        quantita.innerHTML = `${productData['Quantita'][0]} Quantità Disponibili`;
+        categoria.innerHTML = `${productData['Categoria'][0]}`;
+    })
+}
+
+function goback() 
+{
+    maincontainer.style.display = "block";
+    infocontainer.style.display = "none";
 }
